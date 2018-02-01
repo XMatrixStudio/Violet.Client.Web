88b3f100ce980f2e2471af2ec8dbba6b9b79346a
MegaShow <megaxiu@outlook.com>
2018-02-02 00:03:23 +0800

    :construction: Englsih Language Support
    BUG:
    1. 编辑个人资料的生日前面没有文字提示
    2. 如果没有填完信息会没法更新
    3. 更新之后的位置和生日没法设置为private

--- src/store/modules/language.js
+++ src/store/modules/language.js
@@ -26,7 +26,7 @@ const state = {
       Upload: {
         hint: '点击，或拖动图片至此处！',
         loading: '正在上传……',
-        noSupported: '浏览器不支持该功能，请使用IE10以上或其他现在浏览器！',
+        noSupported: '浏览器不支持该功能，请使用IE10以上或其他现代浏览器！',
         success: '上传成功',
         fail: '图片上传失败',
         preview: '头像预览',
@@ -213,6 +213,220 @@ const state = {
         name: 'Violet User System',
         detail: 'Violet 中央授权系统',
         invalidQuery: '参数错误'
+      }
+    },
+    en: {
+      Notice: {
+        failed: 'An error occured.',
+        error: {
+          unknown: 'Unknown error. Please contact with the administrator. Error parameter: ',
+          server: 'Unable to connect to the server, please try again later.',
+          logTimeout: 'Login is out of date. Please login again.'
+        }
+      },
+      User: {
+        home: 'Homepage',
+        setting: 'Settings',
+        website: 'Authorizations',
+        dev: 'App Management',
+        logout: 'Logout',
+        modal: {
+          title: 'Confirm',
+          content: 'Do you really want to logout?',
+          button: 'Logout'
+        },
+        avatar: 'Update your avatar'
+      },
+      Upload: {
+        hint: 'Click or drag the file here to upload',
+        loading: 'Uploading...',
+        noSupported: 'Browser is not supported, please use IE10+ or other browsers',
+        success: 'Upload success',
+        fail: 'Upload failed',
+        preview: 'Preview',
+        btn: {
+          off: 'Cancel',
+          close: 'Close',
+          back: 'Back',
+          save: 'Save'
+        },
+        error: {
+          onlyImg: 'Image only',
+          outOfSize: 'Image exceeds size limit: ',
+          lowestPx: 'Image\'s size is too low. Expected at least: '
+        }
+      },
+      UserInfo: {
+        email: 'Email',
+        url: 'URL',
+        phone: 'Phone',
+        location: 'Location',
+        locationInput: 'The city you live in',
+        birthDate: 'Birthday',
+        bio: 'Bio',
+        className: ['Violet User', 'Developer', 'Administrator', 'Super Administrator', 'Mysterious User'],
+        switchOn: 'Public',
+        switchOff: 'Private',
+        gender: {
+          label: 'Gender',
+          man: 'Male',
+          woman: 'Female',
+          other: 'Other'
+        },
+        edit: 'Edit your profile',
+        submit: 'Confirm edit',
+        success: 'Update success',
+        invalid: 'Invalid format',
+        error: 'Error: ',
+        overflow: 'No more than %d characters'
+      },
+      Login: {
+        error: 'Incorrect username or password, please input again',
+        nullUser: 'Please input your username or email',
+        nullPass: 'Please input your password',
+        userHelp: 'Your username / email',
+        passHelp: 'Your password',
+        login: 'Login',
+        auto: 'Remember me',
+        forget: 'Forgot password?',
+        noAccount: 'No account?',
+        register: 'Register'
+      },
+      Register: {
+        title: 'Use email to register',
+        button: 'Register',
+        hadAccount: 'Having an account',
+        login: 'Login now',
+        success: 'Register success',
+        error: 'Register fail'
+      },
+      Auth: {
+        Hi: 'Hi',
+        confirm: 'Authorize for ',
+        auth: 'Authorize',
+        cancel: 'Cancel',
+        change: 'Change account'
+      },
+      Reset: {
+        title: 'Get back your password',
+        button: 'Reset your password',
+        remember: 'Remembered?',
+        login: 'Login now',
+        success: 'Reset password success, please login again',
+        fail: 'Reset password fail'
+      },
+      Verify: {
+        title: 'Check your email',
+        button: 'Check email',
+        change: 'Change account',
+        success: 'Check email success',
+        fail: 'Check email fail',
+        toLogin: 'Please login'
+      },
+      Form: {
+        register: 'Register',
+        login: 'Login',
+        email: 'Email',
+        userName: 'Username',
+        password: 'Password',
+        passwordCheck: 'Confirm password',
+        vCode: 'Verification code',
+        emailCode: 'Email verification code',
+        getVCode: 'Get verification code',
+        againGetVCode: 'Get code again',
+        sentEmailCode: 'The code has been sent to your email',
+        nullName: 'Please input your username',
+        invalidName: 'Username begins with a letter, including letter, number and underline (maximun is 32 characters)',
+        nullPass: 'Please input your password',
+        lessPass: 'Password can\'t be less than 6 characters',
+        largePass: 'Password can\'t be greater than 128 characters',
+        invalidPassword: 'Invalid password',
+        invalidPass: 'Password can\'t only include number',
+        againPass: 'Please input your password again',
+        errorPass: 'Inconsistent passwords',
+        nullEmail: 'Email can\'t be empty',
+        invalidEmail: 'Invalid email',
+        limitTime: 'Frequent request, please request later',
+        nullVCode: 'Verification code can\'t be empty',
+        timeoutEmailCode: 'Email verification code fails, please get it again',
+        errorEmailCode: 'Error email verification code',
+        invalid: 'Unable to check',
+        errorVCode: 'Error verification code',
+        noExistEmail: 'Email doesn\'t exist',
+        existEmail: 'This email has been used to register, please try to login',
+        existName: 'Username exists',
+        reservedName: 'Username is reserved by system',
+        otherError: 'Unknown error. Please contact with the administrator. Error parameter: ',
+        failVCode: 'Get verification code fail, please get it later'
+      },
+      AuthList: {
+        title: 'Authorized website',
+        none: 'There is no websit authorized',
+        authLogin: 'Authorize to login',
+        cancelAuth: 'Remove the authorization',
+        confirmTitle: 'Confirm to remove the authorization',
+        confirm: 'Do you really want to remove the authorization?',
+        sure: 'Confirm',
+        cancel: 'Cancel'
+      },
+      Dev: {
+        title: 'App management',
+        none: 'There is no application of thee current account',
+        add: 'Create application',
+        content: 'The number of applications: ',
+        content2: 'Do you want to create application?',
+        fail: 'Create application fail',
+        failContent: 'The number of applications has reahced the limit of the current account',
+        sure: 'Confirm',
+        cancel: 'Cancel',
+        detail: 'View the details'
+      },
+      DevDetail: {
+        title: 'APP details',
+        id: 'ID',
+        key: 'Key',
+        sure: 'Confirm',
+        cancel: 'Cancel',
+        keyHelp: 'Suggest to change at regular time to ensure safety',
+        change: 'Change',
+        name: 'APP name',
+        nameHelp: 'The application name is the most intuitive display of your application.',
+        url: 'APP URL',
+        urlHelp: 'The homepage of application',
+        callBack: 'Callback URL',
+        callBackHelp: '登陆后Code返回的地址，你需要设置接受他的页面',
+        detail: 'APP introduction',
+        detailHelp: 'Display at the user\'s loginning',
+        help: 'API documents',
+        submit: 'Edit information',
+        delete: 'Delete APP',
+        nullName: 'Application name can\'t be empty',
+        nullUrl: 'Application URL can\'t be empty',
+        nullCallBack: 'Callback URL can\'t be empty',
+        nullDetail: 'Application introduction can\'t be empty',
+        lengthLimit: 'Length between %1d - %2d',
+        keyTitle: 'Confirm the change of key',
+        keyContent1: 'Changing key will cause immediate failure of the current service',
+        keyContent2: 'The service needs to be redeployed to use the service',
+        keySuccess: 'Change key success',
+        invalidId: 'Invalid application ID',
+        setSuccess: 'Edit information success',
+        deleteTitle: 'Confirm the deletion of application',
+        deleteContent1: 'Do you really want to delete the application?',
+        deleteContent2: 'It will not be restored after deleting.',
+        deleteSuccess: 'Delete application success',
+        iconSuccess: 'Update icon success',
+        icon: 'Change icon',
+        invalidQuery: 'Error parameter'
+      },
+      Index: {
+        welcome: 'You are login to ',
+        violet: 'No, I want to login to Violet',
+        errorClientId: 'Invalid url, you will login to Violet User System.',
+        errorOther: 'Unknown error, error parameter',
+        name: 'Violet User System',
+        detail: 'Violet Central Authorization System',
+        invalidQuery: 'Error parameter'
       }
     }
   }
