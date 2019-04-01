dc934db3d7d40ff0b6da98aa445e59ec80689c74
ZhenlyChen <zhenlychen@foxmail.com>
2019-04-01 22:50:00 +0800

    App Setting

--- src/Pages/User/Containers/Home/Components/Apps/AppDetailComp/AppSetting.tsx
+++ src/Pages/User/Containers/Home/Components/Apps/AppDetailComp/AppSetting.tsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 import './AppSetting.less'
+import { Switch } from 'antd'
 
 class AppSetting extends Component {
   render() {
-    return <div className='app-setting'>应用设置</div>
+    return (
+      <div className='app-setting'>
+        <div className='setting-item'>
+          <div className='item-title'>应用状态</div>
+          <div className='item-more'>你可以随时更改应用工作状态</div>
+          <div className='item-control'>
+            <Switch defaultChecked={true} />
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
