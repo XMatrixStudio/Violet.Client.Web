87d975cb38ff490104831bd416c3949528bc6f03
ZhenlyChen <zhenlychen@foxmail.com>
2019-04-09 19:23:46 +0800

    Member manger

--- src/Pages/User/Containers/Home/Components/Apps/OrganizationComp/MemberCard.tsx
+++ src/Pages/User/Containers/Home/Components/Apps/OrganizationComp/MemberCard.tsx
@@ -8,9 +8,8 @@ interface IMemberCardProps {
   data: {
     name: string
     avatar: string
-    canChange: boolean
-    canUp?: boolean
-    canChat?: boolean
+    type: 'admin' | 'dev' | 'wait' | 'add'
+    isMe?: boolean
   }
 }
 
@@ -25,56 +24,80 @@ class MemberCard extends Component<IMemberCardProps> {
 
   render() {
     return (
-      <div
-        className='base-card-box'
-        onClick={() => {
-          this.showMore = !this.showMore
-        }}
-      >
+      <div className='base-card-box'>
         <div className='member-box'>
           <div className='user-info-avatar'>
             <img className='user-avatar' src={this.props.data.avatar} />
           </div>
-          <div className='user-info-text'>{this.props.data.name}</div>
+          <div
+            className='user-info-text'
+            onClick={() => {
+              this.showMore = !this.showMore
+            }}
+          >
+            <div className='user-nick'>{this.props.data.name}</div>
+            <div className='account-name'>helloWorld</div>
+          </div>
           <div
             className='user-control'
             onClick={e => {
               e.stopPropagation()
             }}
           >
-            {this.props.data.canChange &&
-              (this.props.data.canUp ? (
-                <Tooltip title='升级为管理员'>
-                  <Icon className='change-icon control-icon' type='rocket' />
-                </Tooltip>
-              ) : (
-                <Tooltip title='降级为成员'>
-                  <Icon className='change-icon control-icon' type='bell' />
-                </Tooltip>
-              ))}
-            {this.props.data.canChat === true && (
-              <Tooltip title='发起对话'>
-                <Icon className='message-icon control-icon' type='message' />
-              </Tooltip>
-            )}
-            {this.props.data.canChange && (
-              <Popconfirm
-                title={'是否将' + this.props.data.name + '移出组织'}
-                okType='danger'
-                okText='是'
-                placement='bottom'
-                cancelText='否'
-                onConfirm={() => {
-                  console.log('del')
-                }}
-              >
-                <Tooltip title='移出组织'>
-                  <Icon
-                    className='delete-icon control-icon'
-                    type='user-delete'
-                  />
-                </Tooltip>
-              </Popconfirm>
+            {this.props.data.isMe !== true && (
+              <>
+                {this.props.data.type === 'dev' && (
+                  <Tooltip title='升级为管理员'>
+                    <Icon className='change-icon control-icon' type='rocket' />
+                  </Tooltip>
+                )}
+                {this.props.data.type === 'admin' && (
+                  <Tooltip title='降级为成员'>
+                    <Icon className='change-icon control-icon' type='bell' />
+                  </Tooltip>
+                )}
+                {
+                  <Tooltip title='发起对话'>
+                    <Icon
+                      className='message-icon control-icon'
+                      type='message'
+                    />
+                  </Tooltip>
+                }
+                {this.props.data.type === 'wait' && (
+                  <Tooltip title='取消邀请'>
+                    <Icon
+                      className='disconnect-icon control-icon'
+                      type='disconnect'
+                    />
+                  </Tooltip>
+                )}
+                {this.props.data.type === 'add' && (
+                  <Tooltip title='邀请加入'>
+                    <Icon className='add-icon control-icon' type='user-add' />
+                  </Tooltip>
+                )}
+                {this.props.data.type !== 'wait' &&
+                  this.props.data.type !== 'add' && (
+                    <Popconfirm
+                      title={'是否将' + this.props.data.name + '移出组织'}
+                      okType='danger'
+                      okText='是'
+                      placement='bottom'
+                      cancelText='否'
+                      onConfirm={() => {
+                        console.log('del')
+                      }}
+                    >
+                      <Tooltip title='移出组织'>
+                        <Icon
+                          className='delete-icon control-icon'
+                          type='user-delete'
+                        />
+                      </Tooltip>
+                    </Popconfirm>
+                  )}
+              </>
             )}
           </div>
         </div>
--- src/Pages/User/Containers/Home/Components/Apps/OrganizationComp/OrgMember.tsx
+++ src/Pages/User/Containers/Home/Components/Apps/OrganizationComp/OrgMember.tsx
@@ -2,9 +2,133 @@ import React, { Component } from 'react'
 import './OrgMember.less'
 import TestAvatar from '@/Assets/avatar.jpg'
 import MemberCard from './MemberCard'
-import { Button } from 'antd'
+import { Button, Modal, Input, Icon } from 'antd'
+import { observer } from 'mobx-react'
+import { observable } from 'mobx'
 
+type ListStatus = 'load' | 'nothing' | 'init' | 'show'
+
+@observer
 class OrgMember extends Component {
+  @observable showInvite: boolean
+  @observable listStatus: ListStatus
+
+  searchResult = (
+    <>
+      <MemberCard
+        data={{
+          name: '我是秀秀',
+          avatar: TestAvatar,
+          type: 'add'
+        }}
+      />
+      <MemberCard
+        data={{
+          name: '我也是秀秀',
+          avatar: TestAvatar,
+          type: 'add'
+        }}
+      />
+      <MemberCard
+        data={{
+          name: '我也是秀秀',
+          avatar: TestAvatar,
+          type: 'add'
+        }}
+      />
+      <MemberCard
+        data={{
+          name: '我也是秀秀',
+          avatar: TestAvatar,
+          type: 'add'
+        }}
+      />
+      <MemberCard
+        data={{
+          name: '我也是秀秀',
+          avatar: TestAvatar,
+          type: 'add'
+        }}
+      />
+      <MemberCard
+        data={{
+          name: '我也是秀秀',
+          avatar: TestAvatar,
+          type: 'add'
+        }}
+      />
+    </>
+  )
+
+  searchTimer: NodeJS.Timeout | null
+
+  constructor(props: any) {
+    super(props)
+    this.showInvite = false
+    this.listStatus = 'init'
+    this.searchTimer = null
+  }
+
+  onSearch = (value: string) => {
+    if (this.listStatus !== 'show') {
+      this.listStatus = 'load'
+    }
+    if (value === '') {
+      this.listStatus = 'init'
+    } else if (value === 'xx') {
+      this.listStatus = 'nothing'
+    } else {
+      // 发送请求
+      this.listStatus = 'show'
+    }
+  }
+
+  onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (this.listStatus !== 'show') {
+      this.listStatus = 'load'
+    }
+    if (this.searchTimer) {
+      clearTimeout(this.searchTimer)
+    }
+    const value = event.target.value
+    if (value === '') {
+      this.onSearch('')
+    } else {
+      this.searchTimer = setTimeout(() => {
+        this.onSearch(value)
+      }, 1000)
+    }
+  }
+
+  ListContentBox = (status: ListStatus) => {
+    switch (status) {
+      case 'init':
+        return (
+          <div className='status-list'>
+            <Icon type='search' />
+            <p>搜索用户</p>
+          </div>
+        )
+      case 'nothing':
+        return (
+          <div className='status-list'>
+            <Icon type='inbox' />
+            <p>找不到相关用户</p>
+          </div>
+        )
+
+      case 'load':
+        return (
+          <div className='status-list'>
+            <Icon type='loading' />
+          </div>
+        )
+
+      case 'show':
+        return this.searchResult
+    }
+  }
+
   render() {
     return (
       <div className='org-member'>
@@ -13,7 +137,8 @@ class OrgMember extends Component {
           data={{
             name: 'ZhenlyChen',
             avatar: TestAvatar,
-            canChange: false
+            isMe: true,
+            type: 'admin'
           }}
         />
         <div className='item-title'>管理员</div>
@@ -22,9 +147,7 @@ class OrgMember extends Component {
           data={{
             name: 'MegaShow',
             avatar: TestAvatar,
-            canChange: true,
-            canUp: false,
-            canChat: true
+            type: 'admin'
           }}
         />
         <div className='item-title'>开发者</div>
@@ -32,24 +155,64 @@ class OrgMember extends Component {
           data={{
             name: 'AA',
             avatar: TestAvatar,
-            canChange: true,
-            canUp: true,
-            canChat: true
+            type: 'dev'
           }}
         />
         <MemberCard
           data={{
             name: 'BB',
             avatar: TestAvatar,
-            canChange: true,
-            canUp: true,
-            canChat: true
+            type: 'dev'
+          }}
+        />
+        <div className='item-title'>待确认</div>
+        <MemberCard
+          data={{
+            name: '我好秀啊',
+            avatar: TestAvatar,
+            type: 'wait'
+          }}
+        />
+        <MemberCard
+          data={{
+            name: '楼上太秀了',
+            avatar: TestAvatar,
+            type: 'wait'
           }}
         />
         <div className='more-member'>
-          <Button size='large' type='primary' icon='user-add'>
+          <Button
+            onClick={() => {
+              this.showInvite = true
+            }}
+            size='large'
+            type='primary'
+            icon='user-add'
+          >
             邀请加入
           </Button>
+          <Modal
+            visible={this.showInvite}
+            onOk={() => {
+              this.showInvite = false
+            }}
+            onCancel={() => {
+              this.showInvite = false
+            }}
+            className='model-invite'
+            title='邀请加入XMatrix'
+            okText='完成'
+            cancelText='取消'
+          >
+            <Input.Search
+              onSearch={this.onSearch}
+              onChange={this.onChange}
+              placeholder='通过用户名、邮箱或手机号搜索用户'
+            />
+            <div className='user-list'>
+              {this.ListContentBox(this.listStatus)}
+            </div>
+          </Modal>
         </div>
       </div>
     )
