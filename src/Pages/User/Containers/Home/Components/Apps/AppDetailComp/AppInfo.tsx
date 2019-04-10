346534fdad34a200f1bccec8fdc621ce68be4dc1
ZhenlyChen <zhenlychen@foxmail.com>
2019-04-10 13:16:46 +0800

    Update AppInfo

--- src/Pages/User/Containers/Home/Components/Apps/AppDetailComp/AppInfo.tsx
+++ src/Pages/User/Containers/Home/Components/Apps/AppDetailComp/AppInfo.tsx
@@ -5,6 +5,7 @@ import AvatarSelect from '../../Utils/AvatarSelect'
 import { observable } from 'mobx'
 import { observer, inject } from 'mobx-react'
 import UIStore from 'src/Store/UIStore'
+import AppInfoForm from './AppInfoForm'
 
 interface IAppInfo {
   UIStore?: UIStore
@@ -16,19 +17,23 @@ class AppInfo extends Component<IAppInfo> {
   appIcon?: File
   @observable appInfo: {
     name: string
-    des: string
+    describe: string
     home: string
     callback: string
+    category: string
   }
+  @observable isEdit: boolean
 
   constructor(props: any) {
     super(props)
     this.appInfo = {
       name: 'Violet',
-      des: '描述',
+      describe: '描述',
       home: 'https://xmatrix.studio',
-      callback: 'https://xmatrix.stduio/hello'
+      callback: 'https://xmatrix.stduio/hello',
+      category: 'tool'
     }
+    this.isEdit = false
   }
 
   componentWillMount() {
@@ -55,69 +60,98 @@ class AppInfo extends Component<IAppInfo> {
   }
 
   render() {
+    const InfoShow = (
+      <>
+        <div className='avatar-item'>
+          <AvatarSelect
+            title='点击或拖动选择应用图标'
+            imageURL='https://violet-1252808268.cos.ap-guangzhou.myqcloud.com/0.png'
+            setImage={file => {
+              this.appIcon = file
+            }}
+          />
+        </div>
+        <div className='info-item'>
+          <span className='info-label'>ID：</span>
+          <Tooltip title='点击复制'>
+            <Tag
+              className='tag-label'
+              onClick={() => {
+                message.destroy()
+                message.success('应用ID已复制到剪贴板')
+              }}
+              color='#87d068'
+            >
+              87d06887d06887d068
+            </Tag>
+          </Tooltip>
+        </div>
+        <div className='info-item'>
+          <span className='info-label'>Key：</span>
+          <Tooltip title='点击复制'>
+            <Tag
+              className='tag-label'
+              onClick={() => {
+                message.destroy()
+                message.success('应用Key已复制到剪贴板')
+              }}
+              color='#fcc85d'
+            >
+              fcc85dfcc85dfcc85dfcc85dfcc85dfcc85d
+            </Tag>
+          </Tooltip>
+        </div>
+        <div className='info-item'>
+          <div className='info-label'>名称：</div>
+          <div className='info-box'>{this.appInfo.name}</div>
+        </div>
+        <div className='info-item'>
+          <div className='info-label'>分类：</div>
+          <div className='info-box'>工具</div>
+        </div>
+        <div className='info-item'>
+          <div className='info-label'>描述：</div>
+          <div className='info-box'>{this.appInfo.describe}</div>
+        </div>
+        <div className='info-item'>
+          <div className='info-label'>主页：</div>
+          <div className='info-box'>{this.appInfo.home}</div>
+        </div>
+        <Button
+          className='btn-edit'
+          icon='edit'
+          onClick={() => {
+            this.isEdit = true
+          }}
+        >
+          修改信息
+        </Button>
+        <div className='info-item'>
+          <div className='info-label'>回调地址：</div>
+          <div className='info-box'>{this.appInfo.callback}</div>
+        </div>
+      </>
+    )
+
     return (
       <div className='app-info'>
         <div className='base-card-box'>
-          <div className='avatar-item'>
-            <AvatarSelect
-              title='点击或拖动选择应用图标'
-              imageURL='https://violet-1252808268.cos.ap-guangzhou.myqcloud.com/0.png'
-              setImage={file => {
-                this.appIcon = file
+          {this.isEdit ? (
+            <AppInfoForm
+              initData={{
+                name: this.appInfo.name,
+                describe: this.appInfo.describe,
+                home: this.appInfo.home,
+                callback: this.appInfo.callback,
+                category: this.appInfo.category
+              }}
+              next={isEdit => {
+                this.isEdit = false
               }}
             />
-          </div>
-          <div className='info-item'>
-            <span className='info-label'>ID：</span>
-            <Tooltip title='点击复制'>
-              <Tag
-                className='tag-label'
-                onClick={() => {
-                  message.destroy()
-                  message.success('应用ID已复制到剪贴板')
-                }}
-                color='#87d068'
-              >
-                87d06887d06887d068
-              </Tag>
-            </Tooltip>
-          </div>
-          <div className='info-item'>
-            <span className='info-label'>Key：</span>
-            <Tooltip title='点击复制'>
-              <Tag
-                className='tag-label'
-                onClick={() => {
-                  message.destroy()
-                  message.success('应用Key已复制到剪贴板')
-                }}
-                color='#fcc85d'
-              >
-                fcc85dfcc85dfcc85dfcc85dfcc85dfcc85d
-              </Tag>
-            </Tooltip>
-          </div>
-          <div className='info-item'>
-            <div className='info-label'>名称：</div>
-            <div className='edit-box'>{this.appInfo.name}</div>
-          </div>
-          <div className='info-item'>
-            <div className='info-label'>分类：</div>
-            <div className='edit-box'>工具</div>
-          </div>
-          <div className='info-item'>
-            <div className='info-label'>描述：</div>
-            <div className='edit-box'>{this.appInfo.des}</div>
-          </div>
-          <div className='info-item'>
-            <div className='info-label'>主页：</div>
-            <div className='edit-box'>{this.appInfo.home}</div>
-          </div>
-          <div className='info-item'>
-            <div className='info-label'>回调地址：</div>
-            <div className='edit-box'>{this.appInfo.callback}</div>
-          </div>
-          <Button className='btn-edit'>修改信息</Button>
+          ) : (
+            InfoShow
+          )}
         </div>
       </div>
     )
---
+++ src/Pages/User/Containers/Home/Components/Apps/AppDetailComp/AppInfoForm.tsx
@@ -0,0 +1,171 @@
+import React, { Component } from 'react'
+import './AppInfoForm.less'
+import { Form, Input, Button, Tooltip, Icon, Select } from 'antd'
+import { WrappedFormUtils } from 'antd/lib/form/Form'
+
+interface IAppInfoFormProps {
+  form: WrappedFormUtils
+  initData: {
+    name: string
+    category: string
+    describe: string
+    home: string
+    callback: string
+  }
+  next: (isEdit: boolean) => void
+}
+
+class AppInfoForm extends Component<IAppInfoFormProps> {
+  handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log(values)
+        this.props.next(true)
+      }
+    })
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form
+    const initData = this.props.initData
+    return (
+      <div className='app-info-form'>
+        <Form className='my-form' onSubmit={this.handleSubmit}>
+          <Form.Item
+            label={
+              <span>
+                应用分类
+                <Tooltip title='应用分类信息决定应用会出现在平台应用中心哪个类别的列表中，并在各处展示中出现。'>
+                  <Icon
+                    className='tip-icon'
+                    type='question-circle'
+                    theme='twoTone'
+                    twoToneColor='#b3b3b3'
+                  />
+                </Tooltip>
+              </span>
+            }
+          >
+            {getFieldDecorator('appClass', {
+              rules: [
+                {
+                  required: true,
+                  message: '请选择应用分类'
+                }
+              ],
+              initialValue: initData.category
+            })(
+              <Select style={{ maxWidth: '300px' }}>
+                <Select.Option value='game'>游戏</Select.Option>
+                <Select.Option value='tool'>工具</Select.Option>
+                <Select.Option value='entertainment'>娱乐</Select.Option>
+                <Select.Option value='live'>生活</Select.Option>
+                <Select.Option value='social '>社交</Select.Option>
+              </Select>
+            )}
+          </Form.Item>
+          <Form.Item
+            label={
+              <span>
+                应用名称
+                <Tooltip title='应用名称是应用在平台唯一标识，请慎重填写。'>
+                  <Icon
+                    className='tip-icon'
+                    type='question-circle'
+                    theme='twoTone'
+                    twoToneColor='#b3b3b3'
+                  />
+                </Tooltip>
+              </span>
+            }
+          >
+            {getFieldDecorator('appName', {
+              rules: [
+                {
+                  required: true,
+                  message: '请输入应用名'
+                }
+              ],
+              initialValue: initData.name
+            })(<Input placeholder='应用名称' />)}
+          </Form.Item>
+          <Form.Item label='应用简介'>
+            {getFieldDecorator('appDescribe', {
+              rules: [
+                {
+                  required: true,
+                  message: '请输入应用简介'
+                }
+              ],
+              initialValue: initData.describe
+            })(<Input placeholder='简短地描述你的应用' />)}
+          </Form.Item>
+          <Form.Item
+            label={
+              <span>
+                应用主页
+                <Tooltip title='应用主页会在各处展示中出现'>
+                  <Icon
+                    className='tip-icon'
+                    type='question-circle'
+                    theme='twoTone'
+                    twoToneColor='#b3b3b3'
+                  />
+                </Tooltip>
+              </span>
+            }
+          >
+            {getFieldDecorator('appHome', {
+              rules: [
+                {
+                  required: true,
+                  message: '请输入应用主页'
+                }
+              ],
+              initialValue: initData.home
+            })(<Input placeholder='https://example.com' />)}
+          </Form.Item>
+          <Form.Item
+            label={
+              <span>
+                应用回调域
+                <Tooltip title='调用API时, 回调地址必须属于应用回调域'>
+                  <Icon
+                    className='tip-icon'
+                    type='question-circle'
+                    theme='twoTone'
+                    twoToneColor='#b3b3b3'
+                  />
+                </Tooltip>
+              </span>
+            }
+          >
+            {getFieldDecorator('appCallBack', {
+              rules: [
+                {
+                  required: true,
+                  message: '请输入应用回调域'
+                }
+              ],
+              initialValue: initData.callback
+            })(<Input placeholder='https://example.com/verify' />)}
+          </Form.Item>
+          <Button htmlType='submit' type='primary'>
+            保存
+          </Button>
+          <Button
+            style={{ marginLeft: '20px' }}
+            onClick={() => {
+              this.props.next(false)
+            }}
+          >
+            取消
+          </Button>
+        </Form>
+      </div>
+    )
+  }
+}
+
+export default Form.create()(AppInfoForm)
